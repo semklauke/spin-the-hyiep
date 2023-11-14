9556220526bc7db56e06535247fc57d9d0eab5d2
semklauke <privat@semklauke.de>
2023-11-14 20:54:56 +0100

    adding test data

---
+++ items.js
@@ -0,0 +1,21 @@
+spinwheeldata = {
+    "wins" : [1,2,4,8,16],
+    "time" : ["1/2h", "1h", "2h", "8h", "16h","24h"],
+    "games" : {
+        "minecraft_survival" : {
+            "img" : "./imgs/Minecraft_2009_icon.png",
+            "name" : "Minecraft Survival",
+            "color" : "#9BC06E"
+        },
+        "minecraft_jump" : {
+            "img" : "./imgs/Minecraft_2009_icon.png",
+            "name" : "Minecraft\n Jump'n Run",
+            "color" : "#E88AE6"
+        },
+        "fortnite_normal" : {
+            "img": "./imgs/Fortnite_S1.png",
+            "name": "Fortnite Normal",
+            "color": "#88CFEF"
+        }
+    }
+}
